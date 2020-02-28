f7dcd3178014d0b7a1e2f7487bfc344a3fa2d66e
Peterschmitt <gregoire.peterschmitt@gmail.com>
2020-02-28 17:02:42 +0100

    navigation bar adapting - last coyote

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,16 @@
 import classes from './Toolbar.css';
 import React from 'react';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
         <nav>
-            ...
+            <NavigationItems/>
         </nav>
     </header>
 );
